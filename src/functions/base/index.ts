3bf1df4cfa2a4670b154d3531b4e4c5c98f1b88d
Amir Shrestha <amir@bottle.com.np>
2020-08-30 19:36:30 +0545

    rearranges the queries dir

--- src/functions/base/index.ts
+++ src/functions/base/index.ts
@@ -31,7 +31,7 @@ import {
 // import { simpleGatewayResponse } from '../../__test_cases__/simpleGatewayResponse';
 
 // TODO: if webpack is discontinued figure out a way for module aliasing without webpack or discontinue altogether
-import { echo } from '../../../queries/echo';
+import { echo } from '../../queries/echo';
 import { testCaseSchema } from './schema';
 import { CustomEvent } from './types';
 
--- src/functions/simpleResponse/index.ts
+++ src/functions/simpleResponse/index.ts
@@ -20,7 +20,7 @@ import { getSuccessResponse } from '../../helpers/response';
 
 // TODO: if webpack is discontinued figure out a way for module aliasing without webpack or discontinue altogether
 // import { echo } from '@queries/echo';
-import { echo } from '../../../queries/echo';
+import { echo } from '../../queries/echo';
 import { testSchema } from './schema';
 
 const handler: APIGatewayProxyHandler = async (
